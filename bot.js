01e031fd2766e6a3c26b9e31d1f38844e22b54c4
Crazy99c <41756265+Crazy99c@users.noreply.github.com>
2018-07-26 19:51:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "&";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'zg') {
+        message.reply('pong');
+      }
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
