be8300cb6f6e2da3ee9dc7dc1cb53b55ff899031
Crazy99c <41756265+Crazy99c@users.noreply.github.com>
2018-07-31 21:31:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,6 +101,34 @@ client.on('message', message => {
 
 
 
+client.on('message', msg => {
+        if(msg.author.bot) return;
+      
+        if(msg.content === '#روابط') {
+          client.guilds.forEach(g => {
+      
+            let l = g.id
+            g.channels.get(g.channels.first().id).createInvite({
+              maxUses: 5,
+              maxAge: 86400
+            }).then(i => msg.channel.send(`${g.name} | <https://discord.gg/${i.code}> | ${l}`))
+      
+      
+          })
+        }
+      
+      })
+
+
+
+
+
+
+
+
+
+
+
 
 client.on('message', message => {
 if (message.author.id === client.user.id) return;
